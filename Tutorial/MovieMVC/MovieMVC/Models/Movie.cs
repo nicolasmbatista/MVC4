b33ef130c7ea563a49e223533e5e01281a855684
Nicolas Batista <nicolasmbatista@gmail.com>
2012-12-31 10:49:15 -0300

    Finished Tutorial

--- Tutorial/MovieMVC/MovieMVC/Models/Movie.cs
+++ Tutorial/MovieMVC/MovieMVC/Models/Movie.cs
@@ -1,15 +1,29 @@
 ﻿using System;
 using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace MovieMVC.Models
 {
     public class Movie
     {
         public int ID { get; set; }
+
+        [Required]
+        [StringLength(30)]
         public string Title { get; set; }
+
+        [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+
+        [Required]
         public string Genre { get; set; }
+
+        [Range(1,100)]
+        [DataType(DataType.Currency)]
         public decimal Price { get; set; }
+    
+        [StringLength(5)]
         public string Rating { get; set; }
         
     }
